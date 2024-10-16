aa700f920b3bc22f3571994d3e1726051023ceb4
bellmin0203 <specs0203@gmail.com>
2024-10-16 16:02:34 +0900

    Modify : git 테스트 2

--- app/src/main/java/com/jm/gittest/MainActivity.kt
+++ app/src/main/java/com/jm/gittest/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             GitTestTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Android 2",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }

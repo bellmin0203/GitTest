567b2db0e47cede32832b231f593de52fc5d9670
hjongmin0203 <jongm0203@gmail.com>
2024-10-21 10:21:58 +0900

    Modify : Git Commit Test.. + Test 1 + Test 2

--- app/src/main/java/com/jm/gittest/MainActivity.kt
+++ app/src/main/java/com/jm/gittest/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             GitTestTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android 2",
+                        name = "Git Commit Test",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
